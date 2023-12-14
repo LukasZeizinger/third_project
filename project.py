11e2cc39c28527afd896f3719aabd7d3930f8c06
LukasZeizinger <143009675+LukasZeizinger@users.noreply.github.com>
2023-12-14 18:58:31 +0100

    Update project.py

--- project.py
+++ project.py
@@ -2,10 +2,35 @@ from bs4 import BeautifulSoup
 
 import requests
 
+import pandas as pd
+
+
 url = "https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue"
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, "html")
 
-print(soup)
+
+table = soup.find_all("table")[1]
+
+world_titles = table.find_all("th")
+
+world_table_titles = [title.text.strip() for title in world_titles]
+#print(world_table_titles)
+#3<table class="wikitable sortable jquery-tablesorter">
+#<caption>
+
+df = pd.DataFrame(columns=world_table_titles)
+
+column_data = table.find_all("tr")
+
+for row in column_data[1:]:
+    row_data = row.find_all("td")
+    individual_row_data = [data.text.strip() for data in row_data]
+    lenght = len(df)
+
+    df.loc[lenght] = individual_row_data
+
+print(df)
+df.to_csv(r"C:\Users\Lukas\Desktop\Python-output\Companies.csv", index = False)
